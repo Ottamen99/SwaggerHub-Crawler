508092e30329304c42265269eccc37568b35ddcd
Ottavio Buonomo <buonoo@usi.ch>
2023-04-18 13:10:55 +0200

    Update mongo connector with retry

--- swagger_scheduler/db/mongoConnector.js
+++ swagger_scheduler/db/mongoConnector.js
@@ -3,15 +3,29 @@ const { MongoClient } = require('mongodb');
 
 
 // create a new MongoClient
-const client = new MongoClient(config.URI, { directConnection: true });
+const client = new MongoClient(config.URI, { directConnection: true, useNewUrlParser: true, useUnifiedTopology: true });
 
-// connect to the server
-client.connect().then(r => {
-    console.log('Connected to MongoDB');
-}).catch(err => {
-    console.log('Error connecting to MongoDB', err);
-});
+let connectToMongo = async (retryInterval = 5000) => {
+    let attempt = 1;
+    while (true) {
+        try {
+            await client.connect();
+            console.log('Connected to MongoDB');
+            return;
+        } catch (err) {
+            console.log(`Error connecting to MongoDB (attempt ${attempt}):`);
+            await new Promise(resolve => setTimeout(resolve, retryInterval));
+        }
+        attempt++;
+    }
+}
+
+(async () => {
+    await connectToMongo();
+})();
 
 module.exports = () => {
     return client.db(config.DATABASE_NAME);
 };
+
+module.exports.connectToMongo = connectToMongo;
--- swagger_scraper/db/mongoConnector.js
+++ swagger_scraper/db/mongoConnector.js
@@ -3,17 +3,29 @@ const { MongoClient } = require('mongodb');
 
 
 // create a new MongoClient
-const client = new MongoClient(config.URI, {connectTimeoutMS: 100000, directConnection: true, useUnifiedTopology: true});
+const client = new MongoClient(config.URI, { directConnection: true, useNewUrlParser: true, useUnifiedTopology: true });
 
-// connect to the server
-client.connect((err) => {
-    if (err) {
-        console.log('Error connecting to MongoDB', err);
-    } else {
-        console.log('Connected to MongoDB');
+let connectToMongo = async (retryInterval = 5000) => {
+    let attempt = 1;
+    while (true) {
+        try {
+            await client.connect();
+            console.log('Connected to MongoDB');
+            return;
+        } catch (err) {
+            console.log(`Error connecting to MongoDB (attempt ${attempt}):`);
+            await new Promise(resolve => setTimeout(resolve, retryInterval));
+        }
+        attempt++;
     }
-});
+}
+
+(async () => {
+    await connectToMongo();
+})();
 
 module.exports = () => {
     return client.db(config.DATABASE_NAME);
 };
+
+module.exports.connectToMongo = connectToMongo;
