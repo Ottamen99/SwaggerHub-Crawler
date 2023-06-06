a8efdfd9de7a2efb2990149d3cb3ebcac487d5ba
Ottavio Buonomo <buonoo@usi.ch>
2023-06-06 11:45:13 +0200

    Cleaning utility functions

--- swagger_url_retriever/utils/utilityFunctions.js
+++ swagger_url_retriever/utils/utilityFunctions.js
@@ -1,52 +1,30 @@
 const crypto = require('crypto');
-const {BASE_SWAGGER_PROXY_URL} = require("../config/config");
 
-// create delay function with random delay
-exports.randomDelay = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-// create function that hash a string with sha256
+/**
+ * Hashes a string using sha256
+ * @param str - string to hash
+ * @returns {string} - hashed string
+ */
 exports.hashString = (str) => {
     const hash = crypto.createHash('sha256');
     hash.update(str);
     return hash.digest('hex');
 }
 
-// parse swaggerhub url to get the owner name
+/**
+ * Parses the owner from a SwaggerHub url
+ * @param url - SwaggerHub url
+ * @returns {*} - owner
+ */
 exports.parseOwner = (url) => {
     return url.split('/')[4];
 }
 
-// parse swaggerhub url to get the base url that is everything except the version
+/**
+ * Get API url without the version
+ * @param url - SwaggerHub url
+ * @returns {string} - API url without the version
+ */
 exports.getBaseURL = (url) => {
     return url.split('/').slice(0, -1).join('/');
-}
-
-exports.buildUrl = (sortBy= undefined,
-                order= undefined,
-                limit= undefined,
-                page= undefined,
-                owner= undefined,
-                spec= undefined) => {
-    let url = BASE_SWAGGER_PROXY_URL
-    if (sortBy) {
-        url += `sort=${sortBy}&`
-    }
-    if (order) {
-        url += `order=${order}&`
-    }
-    if (limit) {
-        url += `limit=${limit}&`
-    }
-    if (page) {
-        url += `page=${page}&`
-    }
-    if (owner) {
-        url += `owner=${owner}&`
-    }
-    if (spec) {
-        url += `spec=${spec}&`
-    }
-    return url
 }
